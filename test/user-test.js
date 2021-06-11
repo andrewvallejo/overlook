5463f217a0d4f1e85101d63d6f527545e9377376
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-11 14:48:49 -0600

    Add id property test

--- test/user-test.js
+++ test/user-test.js
@@ -3,17 +3,23 @@ import User from '../src/components/classes/User'
 import { userData } from './sample-data'
 
 describe('User', () => {
-  let guest1, guest2, guest3, guest4, guest5
+  let guest1, guest2, guest3, guest4, guest5;
   beforeEach(() => {
-    guest1 = new User(userData[4])
-    guest2 = new User(userData[0])
-    guest3 = new User(userData[1])
-    guest4 = new User(userData[2])
-    guest5 = new User(userData[3])
+    guest1 = new User(userData[0])
+    guest2 = new User(userData[1])
+    guest3 = new User(userData[2])
+    guest4 = new User(userData[3])
+    guest5 = new User(userData[4])
   })
   it('should be an instance of User', function() {
     expect(guest1).to.be.an.instanceOf(User)
   })
+  it('should have an id', () => {
+    expect(guest1.id).to.be.equal(1)
+    expect(guest3.id).to.be.equal(3)
+    expect(guest5.id).to.be.equal(5)
+  })
 })
+
 
 

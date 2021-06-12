fcf6a58f63d42990011b13777baf607df9905362
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-11 20:55:16 -0600

    Refactor functions to be variables

--- test/hotel-sample-data.js
+++ test/hotel-sample-data.js
@@ -3,25 +3,21 @@ import Room from '../src/components/classes/Room'
 import Booking from '../src/components/classes/Booking'
 
 let roomCount = 0
-const instantiateRooms = () => {
-  return roomsData.reduce((hotel, room) => {
-    roomCount++
-    const newRoom = new Room(room)
-    hotel[`room${roomCount}`] = newRoom
-    console.log(hotel)
-    return hotel
-  }, {})
-}
+const instantiatedRooms = roomsData.reduce((hotel, room) => {
+  roomCount++
+  const newRoom = new Room(room)
+  hotel[`room${roomCount}`] = newRoom
+  return hotel
+}, {})
+
 
 let bookingCount = 0
-const instantiateBookings = () => {
-  return bookingsData.reduce((hotel, booking) => {
-    bookingCount++
-    const newBooking = new Booking(booking)
-    hotel[`booking${bookingCount}`] = newBooking
-    console.log(hotel)
-    return hotel
-  }, {})
-}
+const instantiatedBookings = bookingsData.reduce((hotel, booking) => {
+  bookingCount++
+  const newBooking = new Booking(booking)
+  hotel[`booking${bookingCount}`] = newBooking
+  return hotel
+}, {})
 
-export {instantiateRooms as rooms, instantiateBookings as bookings}
+
+export {instantiatedRooms as rooms, instantiatedBookings as bookings}

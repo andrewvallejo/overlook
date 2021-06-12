04aff1b192285ef04482a379c72687232c75d670
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-11 18:48:17 -0600

    Add id test

--- test/booking-test.js
+++ test/booking-test.js
@@ -1,8 +1,8 @@
-import { expect } from "chai";
-import Booking from "../src/components/classes/Booking";
-import { bookingsData } from "./sample-data";
+import { expect } from 'chai';
+import Booking from '../src/components/classes/Booking';
+import { bookingsData } from './sample-data';
 
-describe.only("booking", () => {
+describe.only('booking', () => {
   let booking1, booking2, booking3, booking4, booking5;
   beforeEach(() => {
     booking1 = new Booking(bookingsData[0]);
@@ -11,7 +11,11 @@ describe.only("booking", () => {
     booking4 = new Booking(bookingsData[3]);
     booking5 = new Booking(bookingsData[4]);
   });
-  it("should be an instance of booking", () => {
+  it('should be an instance of booking', () => {
     expect(booking1).to.be.an.instanceOf(Booking)
   });
+  it('should have an id', () => {
+    expect(booking1.id).to.be.equal('5fwrgu4i7k55hl6sz')
+    expect(booking3.id).to.be.equal('5fwrgu4i7k55hl6t6')
+  })
 });

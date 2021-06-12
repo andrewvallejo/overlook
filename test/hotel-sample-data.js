3ab5617023622f0b80f864fffe9dc34134592b1c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-11 20:06:13 -0600

    Add import and export declarations

---
+++ test/hotel-sample-data.js
@@ -0,0 +1,7 @@
+import {roomsData, bookingsData} from './sample-data'
+import Room from '../src/components/classes/Room'
+import Booking from '../src/components/classes/Booking'
+
+
+
+const export {instantiateRooms as rooms, instantiateBookings as bookings}

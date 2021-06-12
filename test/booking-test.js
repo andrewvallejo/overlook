e6e8a9ec912ba7bdd645a41f296e0f6149903e24
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-11 20:06:29 -0600

    Remove only from unit test

--- test/booking-test.js
+++ test/booking-test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import Booking from '../src/components/classes/Booking';
 import { bookingsData } from './sample-data';
 
-describe.only('booking', () => {
+describe('booking', () => {
   let booking1, booking2, booking3, booking4, booking5;
   beforeEach(() => {
     booking1 = new Booking(bookingsData[0]);

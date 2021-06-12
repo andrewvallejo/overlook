2387aefcbb8c619cb525f2188bfae10bf94efc3e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-12 14:09:33 -0600

    Add all rooms and booking instances from data

--- test/hotel-test.js
+++ test/hotel-test.js
@@ -2,20 +2,27 @@ import { expect } from 'chai';
 import Hotel from '../src/components/classes/Hotel';
 import {rooms, bookings} from './data/hotel-sample-data'
 
-describe('Booking', () => {
+describe.only('Booking', () => {
   let hotel
-  let room1, room2, room3
-  let booking1, booking2, booking3
+  let room1, room2, room3, room4, room5
+  let booking1, booking2, booking3, booking4, booking5
+  const hotelRooms = [room1, room2, room3, room4, room5]
+  const hotelBookings = [booking1, booking2, booking3, booking4, booking5]
   beforeEach(() => {
     room1 = rooms.room1
     room2 = rooms.room2
     room3 = rooms.room3
+    room4 = rooms.room4
+    room5 = rooms.room5
     booking1 = bookings.book1
     booking2 = bookings.book2
     booking3 = bookings.book3
+    booking4 = bookings.book4
+    booking5 = bookings.book5
     hotel = new Hotel()
   })
   it('should be an instantation of Hotel', () => {
     expect(hotel).to.be.an.instanceOf(Hotel)
   })
 });
+

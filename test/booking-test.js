852653e51442d9693f22838493f9f57388e4bf00
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-11 18:25:46 -0600

    Add test skeleton

--- test/booking-test.js
+++ test/booking-test.js
@@ -1,8 +1,17 @@
-import chai from 'chai';
-const expect = chai.expect;
+import { expect } from "chai";
+import Booking from "../src/components/classes/Booking";
+import { bookingsData } from "./sample-data";
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe.only("booking", () => {
+  let booking1, booking2, booking3, booking4, booking5;
+  beforeEach(() => {
+    booking1 = new Booking(bookingsData[0]);
+    booking2 = new Booking(bookingsData[1]);
+    booking3 = new Booking(bookingsData[2]);
+    booking4 = new Booking(bookingsData[3]);
+    booking5 = new Booking(bookingsData[4]);
+  });
+  it("should be an instance of booking", () => {
+    expect(booking1).to.be.an.instanceOf(Booking)
   });
 });

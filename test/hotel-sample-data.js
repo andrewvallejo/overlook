4d5d6487fd044db0edde6e9f199ceade58effcde
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-11 20:07:03 -0600

    Add function to instantiate from sample data

--- test/hotel-sample-data.js
+++ test/hotel-sample-data.js
@@ -2,6 +2,17 @@ import {roomsData, bookingsData} from './sample-data'
 import Room from '../src/components/classes/Room'
 import Booking from '../src/components/classes/Booking'
 
+let roomCount = 0
+const instantiateRooms = () => {
+  return roomsData.reduce((hotel, room) => {
+    roomCount++
+    const newRoom = new Room(room)
+    hotel[`room${roomCount}`] = newRoom
+    console.log(hotel)
+    return hotel
+  }, {})
+}
+
 
 
 const export {instantiateRooms as rooms, instantiateBookings as bookings}

b5753aff2524f462508448b8528f70641d7e5efe
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-12 18:35:12 -0600

    Add all default properties to Hotel class

--- src/components/classes/Hotel.js
+++ src/components/classes/Hotel.js
@@ -3,7 +3,12 @@
 
 class Hotel {
   constructor() {
-
+    this.date = '2000/12/25',
+    this.rooms = [],
+    this.bookings = [],
+    this.availableRooms = [],
+    this.pendingBookings = []
+    this.availability = 0
   }
 }
 

8eab1cced9b555e02fab715e4a21628374dd4b7e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-12 23:05:12 -0600

    Refactor before today date test and bookings

--- test/hotel-test.js
+++ test/hotel-test.js
@@ -12,11 +12,11 @@ describe.only('Booking', () => {
     room3 = rooms.room3
     room4 = rooms.room4
     room5 = rooms.room5
-    booking1 = bookings.book1
-    booking2 = bookings.book2
-    booking3 = bookings.book3
-    booking4 = bookings.book4
-    booking5 = bookings.book5
+    booking1 = bookings.booking1
+    booking2 = bookings.booking2
+    booking3 = bookings.booking3
+    booking4 = bookings.booking4
+    booking5 = bookings.booking5
     hotelRooms = [room1, room2, room3, room4, room5]
     hotelBookings = [booking1, booking2, booking3, booking4, booking5]
     hotel = new Hotel()
@@ -48,26 +48,25 @@ describe.only('Booking', () => {
     expect(hotel.rooms).to.be.lengthOf(5)
     expect(hotel.rooms).to.be.deep.equal(hotelRooms)
   })
-  it.skip('should be able to instaniate all of the bookings', () => {
+  it('should be able to instaniate all of the bookings', () => {
     expect(hotel.bookings).to.be.deep.equal([])
-    hotel.generateBookings()
+    hotel.generateBookings(hotelBookings)
     expect(hotel.bookings).to.be.lengthOf(5)
     expect(hotel.bookings).to.be.deep.equal(hotelBookings)
   })
-  it.skip('should be to update date property to current date', () => {
+  it('should be able update the date to today', () => {
     expect(hotel.date).to.be.equal('2000/12/25')
-    hotel.fetchCurrentDate()
+    hotel.selectDate(today)
     expect(hotel.date).to.be.equal(today)
   })
-  it.skip('should be able to update the date to a future date', () => {
+  it('should be able to update the date to a future date', () => {
     expect(hotel.date).to.be.equal('2000/12/25')
     hotel.selectDate('2022/03/09')
     expect(hotel.date).to.be.equal('2022/03/09')
   })
-  it.skip('should not be able to update the date to a before today', () => {
-    expect(hotel.date).to.be.equal('2000/12/25')
+  it('should not be able to update the date to a before today', () => {
     hotel.selectDate('2019/11/22')
-    expect(hotel.date).to.be.equal('2000/12/25')
+    expect(hotel.date).to.be.equal(today)
   })
 });
 

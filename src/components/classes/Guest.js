28736a0dede6b98a1a4a11ac3bad3274387bda1a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-13 22:53:21 -0600

    Add guest properties

--- src/components/classes/Guest.js
+++ src/components/classes/Guest.js
@@ -1,8 +1,12 @@
 import { User } from './User'
-// import Hotel from './Hotel'
+import Hotel from './Hotel'
 
 export class Guest extends User {
   constructor(user) {
     super(user) 
+    this.guestBookings = []
+    this.valuation = 0
+    this.overLook;
   }
+
 }

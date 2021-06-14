c4c36a6c82a401277d67e4b20acbcc256d4f9ef1
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-14 16:11:22 -0600

    Add dynamic API calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,20 +1,12 @@
-import { User } from './components/classes/User'
-import { Room } from './components/classes/Room'
-import { Bookings } from './components/classes/Booking'
-
-let usersData, roomsData, bookingsData
-
-let users = () => fetch("http://localhost:3001/api/v1/customers")
+const fetchData = (query) => fetch(`http://localhost:3001/api/v1/${query}`)
   .then(response => response.json())
-  .catch(err => console.log('error'));
-let rooms = () => fetch("http://localhost:3001/api/v1/ingredients")
-  .then(response => response.json())
-  .catch(err => console.log('error'));
-let bookings = () => fetch("http://localhost:3001/api/v1/recipes")
-  .then(response => response.json())
-  .catch(err => console.log('error'));
+  .catch(err => console.log('error'))
 
-export const retrieveData = () => {
-  return Promise.all([users(), rooms(), bookings()])
+export const fetchHotelData = () => {
+  return Promise.all([
+    fetchData('customers'), 
+    fetchData('rooms'), 
+    fetchData('bookings')
+  ])
 }
 

9e7a9d96aeec81ef5f07df8af132572faf67f542
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-14 16:05:58 -0600

    Add script to generate guests, rooms and bookings

--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,15 @@
 import './css/styles.scss';
+import {fetchData} from './apiCalls'
+import { Guest } from './components/classes/Guest'
 
+let guestBook
+
+window.onload = () => {
+  fetchData()
+    .then(promise => {
+      guestBook = promise[0].customers.map(user => new Guest(user))
+      guestBook.map(guest => {
+        guest.generateHotel(promise[1].rooms, promise[2].bookings)
+      })
+    })
+}

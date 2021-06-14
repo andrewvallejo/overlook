10c0152424a7a0a25ce517d6bec80921fcb74e58
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-13 22:10:37 -0600

    Update test skeleton with correct test frame

--- test/guest-test.js
+++ test/guest-test.js
@@ -1,8 +1,14 @@
-import chai from 'chai';
-const expect = chai.expect;
+import { expect } from 'chai';
+import { Guest } from '../src/components/classes/Guest';
+import {rooms, bookings, today} from './data/hotel-sample-data'
+import { userData } from './data/sample-data'
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
-  });
-});
+describe.only('Guest', () => {
+  let guest1
+  beforeEach(() => {
+    guest1 = new Guest(userData[0])
+  })
+  it('should be an instantation of a Guest', () => {
+    expect(guest1).to.be.an.instanceOf(Guest)
+  })
+})

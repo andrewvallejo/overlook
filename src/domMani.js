5471f212e3e4a2b3ddcf28872f644537f98df59c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-14 20:35:29 -0600

    Add logic draft for instantiating rooms by date

--- src/domMani.js
+++ src/domMani.js
@@ -1,22 +1,51 @@
+import { today } from './components/utility/getToday'
 // query selectors
-
 const availableRoomsToday = document.querySelector('#availableRoomsToday');
 
-// global variables
+
+
+
 export const retrieveBook = (guestBook) => {
-  analyzeBook(guestBook[0])
+  // let guest = guestBook[0]
+  // renderTodaysRooms(guestBook)
+  renderRoomsByDate(guestBook)
 }
 
-const analyzeBook = (guest) => {
-  let rooms = guest.overlook.rooms
-  let bookings = guest.overlook.bookings
-  renderRooms(rooms)
+
+
+// const renderTodaysRooms = (guestbook) => {
+//   let guest = guestbook[0]
+//   guest.overlook.selectDate(today)
+//   guest.overlook.findAvailableRooms() 
+//   let availableRooms = [...new Set(guest.overlook.availableRooms)]
+//   availableRooms.forEach(room => {
+//     availableRoomsToday.innerHTML += `     
+//     <li aria-label="Room and their properties">
+//     <p>Room number: <span>${room.number}</span></p>
+//     <p>Room type: <span>${room.roomType}</span></p>
+//     <p>Bidet: <span>${room.bidet}</span></p>
+//     <p>Bed Size: <span>${room.bedSize}</span></p>
+//     <p>Number of beds: <span>${room.numBeds}</span></p>
+//     <p>Cost per night: $<span>${room.costPerNight.toFixed(2) }</span></p>
+//   </li>`
+//   }) 
+// } 
+
+
+const renderRoomsByDate = (guestBook) => {
+  let guest = guestBook[0]
+  guest.filterRoomsByDate('2020/02/04')
+  console.log(guest.overlook.availableRooms)
+  // let availableRooms = [...new Set(guest.overlook.availableRooms)]
+  guest.overlook.availableRooms.forEach(room => {
+    availableRoomsToday.innerHTML += `     
+    <li aria-label="Room and their properties">
+    <p>Room number: <span>${room.number}</span></p>
+    <p>Room type: <span>${room.roomType}</span></p>
+    <p>Bidet: <span>${room.bidet}</span></p>
+    <p>Bed Size: <span>${room.bedSize}</span></p>
+    <p>Number of beds: <span>${room.numBeds}</span></p>
+    <p>Cost per night: $<span>${room.costPerNight.toFixed(2) }</span></p>
+  </li>`
+  }) 
 }
-
-const renderRooms = (rooms) => {
-  
-} 
-
-
-
-

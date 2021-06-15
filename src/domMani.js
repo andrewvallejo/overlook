15b2083a198cd735293c1970eaf981746c7100e9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-14 18:27:39 -0600

    Add dom manipulationg setup

--- src/domMani.js
+++ src/domMani.js
@@ -1,6 +1,22 @@
+// query selectors
 
+const availableRoomsToday = document.querySelector('#availableRoomsToday');
 
-export const getbook = (guestBook) => {
-  console.log(guestBook)
+// global variables
+export const retrieveBook = (guestBook) => {
+  analyzeBook(guestBook[0])
 }
+
+const analyzeBook = (guest) => {
+  let rooms = guest.overlook.rooms
+  let bookings = guest.overlook.bookings
+  renderRooms(rooms)
+}
+
+const renderRooms = (rooms) => {
+  
+} 
+
+
+
 

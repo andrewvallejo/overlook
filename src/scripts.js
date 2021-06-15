53d19174b78fbcda5346713bed889eaf79268450
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-14 20:34:30 -0600

    Add logic that filters booking based on date

--- src/scripts.js
+++ src/scripts.js
@@ -2,8 +2,9 @@ import './css/styles.scss';
 import {fetchHotelData} from './apiCalls'
 import { Guest } from './components/classes/Guest'
 import { retrieveBook } from './domMani'
+import { today } from './components/utility/getToday'
 
-export let guestBook, sendData
+export let guestBook, sendData, allBookings
 
 window.onload = () => {
   fetchHotelData()
@@ -11,11 +12,20 @@ window.onload = () => {
 
 fetchHotelData()
   .then(promise => {
-    guestBook = promise[0].customers.map(user => new Guest(user))
+    guestBook = promise[0].customers.map(user => new Guest(user)) 
+    allBookings = promise[2].bookings
+    const filteredBookings = filterBookingsByDate(allBookings, '2020/02/04')
     guestBook.map(guest => {
-      return guest.generateHotel(promise[1].rooms, promise[2].bookings)
+      return guest.generateHotel(promise[1].rooms, filteredBookings)
     })
     retrieveBook(guestBook)
   })
-
+  
+const filterBookingsByDate = (bookings, date)  => {
+  return bookings.filter(booking => {
+    if (booking.date.includes(date)) {
+      return booking
+    }
+  })
+}
   

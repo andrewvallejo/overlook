5af8cd868fd177fb0ff72b3f5a3f3ef32cc7be52
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-15 13:21:17 -0600

    Refactor both hide and show files to be one

--- src/components/utility/hide.js
+++
@@ -1,9 +0,0 @@
-export const hide = (e) =>{
-  if (e.length > 0) {
-    e.forEach((element) => {
-      element.classList.add('hidden')
-    })
-  } else {
-    e.classList.add('hidden')
-  }
-}
--- src/components/utility/hideShow.js
+++ src/components/utility/hideShow.js
@@ -1,3 +1,13 @@
+export const hide = (e) =>{
+  if (e.length > 0) {
+    e.forEach((element) => {
+      element.classList.add('hidden')
+    })
+  } else {
+    e.classList.add('hidden')
+  }
+}
+
 export const show = (e) => {
   if (e.length > 0) {
     e.forEach((element) => {

042241aa1b04ad1862645b980ede9ad92d05ffc5
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-15 15:22:47 -0600

    Refactor code for cleaniness

--- src/domMani.js
+++ src/domMani.js
@@ -3,14 +3,14 @@ import { today } from './components/utility/getToday'
 import { hide, show } from './components/utility/hideShow'
 
 // query selectors
-const availableRoomsToday = document.querySelector('#availableRoomsToday');
 const portal = document.querySelector('#portal')
 const availableRoomsView = document.querySelector('#availableRoomsView')
 
 export const retrieveBook = (guestBook, selectedDate) => {
+  console.log(selectedDate)
   prerenderRoom(guestBook, 'Date', selectedDate)
-  hideView(selectedDate)
-  showView(selectedDate)
+  hideView()
+  showView()
 }
 
 const prerenderRoom = (guestBook, filter, query) => {
@@ -39,50 +39,10 @@ const renderRooms = (availableRooms) => {
   }) 
 }
 
-const hideView = (selectedDate) => {
-  if (selectedDate === today) {
-    hide([portal])
-  }
+const hideView = () => {
+  hide(portal)
 }
 
-const showView = (selectedDate) => {
-  if (selectedDate === today) {
-    show(availableRoomsView)
-  }
+const showView = () => {
+  show(availableRoomsView)
 }
-
-// const renderTodaysRooms = (guestbook) => {
-//   let guest = guestbook[0]
-//   guest.overlook.selectDate(today)
-//   guest.overlook.findAvailableRooms() 
-//   let availableRooms = [...new Set(guest.overlook.availableRooms)]
-//   availableRooms.forEach(room => {
-//     availableRoomsToday.innerHTML += `     
-//     <li aria-label="Room and their properties">
-//     <p>Room number: <span>${room.number}</span></p>
-//     <p>Room type: <span>${room.roomType}</span></p>
-//     <p>Bidet: <span>${room.bidet}</span></p>
-//     <p>Bed Size: <span>${room.bedSize}</span></p>
-//     <p>Number of beds: <span>${room.numBeds}</span></p>
-//     <p>Cost per night: $<span>${room.costPerNight.toFixed(2) }</span></p>
-//   </li>`
-//   }) 
-// } 
-
-
-// const renderRoomsByType = (guestBook) => {
-//   availableRoomsToday.innerHTML = ``
-//   let guest = guestBook[0]
-//   guest.filterRoomsByType('suite') 
-//   guest.overlook.availableRooms.forEach(room => {
-//     availableRoomsToday.innerHTML += `     
-//     <li aria-label="Room and their properties">
-//     <p>Room number: <span>${room.number}</span></p>
-//     <p>Room type: <span>${room.roomType}</span></p>
-//     <p>Bidet: <span>${room.bidet}</span></p>
-//     <p>Bed Size: <span>${room.bedSize}</span></p>
-//     <p>Number of beds: <span>${room.numBeds}</span></p>
-//     <p>Cost per night: $<span>${room.costPerNight.toFixed(2) }</span></p>
-//   </li>`
-//   }) 
-// }

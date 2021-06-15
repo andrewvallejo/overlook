316d21ac091ead9a0746dbe2f6ffdee1657f38f8
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-15 09:02:36 -0600

    Add logic to render by date and type

--- src/domMani.js
+++ src/domMani.js
@@ -8,9 +8,32 @@ const availableRoomsToday = document.querySelector('#availableRoomsToday');
 export const retrieveBook = (guestBook) => {
   // let guest = guestBook[0]
   // renderTodaysRooms(guestBook)
-  renderRoomsByDate(guestBook)
+  // renderRoomsByDate(guestBook)
+  // renderRoomsByType(guestBook)
+  renderRoom(guestBook, 'Date', '2020/04/22')
 }
 
+
+const renderRoom = (guestBook, filter, query) => {
+  availableRoomsToday.innerHTML = ''
+  let guest = guestBook[0]
+  let availableRooms = guest.overlook.availableRooms
+  filter === 'Date' ? guest.filterRoomsByDate(query) : 
+    guest.filterRoomsByType(query)
+  // !guest.overlook.pending ? availableRooms = guest.overlook.rooms : null
+  console.log(availableRooms)
+  availableRooms.forEach(room => {
+    availableRoomsToday.innerHTML += `     
+    <li aria-label="Room and their properties">
+    <p>Room number: <span>${room.number}</span></p>
+    <p>Room type: <span>${room.roomType}</span></p>
+    <p>Bidet: <span>${room.bidet}</span></p>
+    <p>Bed Size: <span>${room.bedSize}</span></p>
+    <p>Number of beds: <span>${room.numBeds}</span></p>
+    <p>Cost per night: $<span>${room.costPerNight.toFixed(2) }</span></p>
+  </li>`
+  }) 
+}
 
 
 // const renderTodaysRooms = (guestbook) => {
@@ -32,20 +55,19 @@ export const retrieveBook = (guestBook) => {
 // } 
 
 
-const renderRoomsByDate = (guestBook) => {
-  let guest = guestBook[0]
-  guest.filterRoomsByDate('2020/02/04')
-  console.log(guest.overlook.availableRooms)
-  // let availableRooms = [...new Set(guest.overlook.availableRooms)]
-  guest.overlook.availableRooms.forEach(room => {
-    availableRoomsToday.innerHTML += `     
-    <li aria-label="Room and their properties">
-    <p>Room number: <span>${room.number}</span></p>
-    <p>Room type: <span>${room.roomType}</span></p>
-    <p>Bidet: <span>${room.bidet}</span></p>
-    <p>Bed Size: <span>${room.bedSize}</span></p>
-    <p>Number of beds: <span>${room.numBeds}</span></p>
-    <p>Cost per night: $<span>${room.costPerNight.toFixed(2) }</span></p>
-  </li>`
-  }) 
-}
+// const renderRoomsByType = (guestBook) => {
+//   availableRoomsToday.innerHTML = ``
+//   let guest = guestBook[0]
+//   guest.filterRoomsByType('suite') 
+//   guest.overlook.availableRooms.forEach(room => {
+//     availableRoomsToday.innerHTML += `     
+//     <li aria-label="Room and their properties">
+//     <p>Room number: <span>${room.number}</span></p>
+//     <p>Room type: <span>${room.roomType}</span></p>
+//     <p>Bidet: <span>${room.bidet}</span></p>
+//     <p>Bed Size: <span>${room.bedSize}</span></p>
+//     <p>Number of beds: <span>${room.numBeds}</span></p>
+//     <p>Cost per night: $<span>${room.costPerNight.toFixed(2) }</span></p>
+//   </li>`
+//   }) 
+// }

2f7a88e39a92d00666c92f4529187eb67ccae2ef
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-06-16 16:04:34 -0600

    Refactor code to make bookingMsg to be dynamic

--- src/domMani.js
+++ src/domMani.js
@@ -3,13 +3,16 @@ import { hide, show } from './components/utility/hideShow'
 
 // query selectors
 const home = document.querySelector('#home')
+const btnSortByType = document.querySelector('#btnSortByType')
+const altMsg = document.querySelector('#altMsg')
+const footerInfo = document.querySelector('#footerInfo')
 const portal = document.querySelector('#portal')
 const availableRoomsView = document.querySelector('#availableRoomsView')
 const guestMenu = document.querySelector('#guestMenu')
 const viewCalendar = document.querySelector('#viewCalendar')
 const menuHeader = document.querySelector('#menuHeader')
 const dynamicMsg = document.querySelector('#dynamicMsg')
-const bookingMsg = document.querySelector('#bookingMsg')
+const altView = document.querySelector('#altView')
 const loginPortal = document.querySelector('#loginPortal')
 
 home.addEventListener('click', (event) => {
@@ -44,7 +47,7 @@ const fetchGuestBookings = (guestBook, bookings) => {
   })
 }
 
-const prerenderRoom = (guestBook, filter, query) => {
+export const prerenderRoom = (guestBook, filter, query) => {
   availableRoomsView.innerHTML = ''        
   let guest = guestBook[0]
   let availableRooms = guest.overlook.availableRooms
@@ -59,12 +62,13 @@ const prerenderRoom = (guestBook, filter, query) => {
   renderRooms(availableRooms, filter)
   renderMsg(filter, guest) 
 }
-//     ${(room.date && (`<p>You booked this room for:  <span>${room.date}</span></p>`)) || null}
 
 const renderRooms = (availableRooms, filter) => {
   let roomDate 
+  hide(footerInfo)
+  show(btnSortByType)
   availableRooms.forEach(room => {
-    filter === 'myBookings' ? roomDate = `${(room.date && (`<p>You booked this room for:  <span>${room.date}</span></p>`)) || ''}`  : roomDate = ''
+    filter === 'myBookings' ? (roomDate = `${(room.date && (`<p>You booked this room for:  <span>${room.date}</span></p>`)) || ''}`, show(footerInfo), hide(btnSortByType))  : roomDate = ''
     availableRoomsView.innerHTML += `
     <article id="${room.number}" tabindex="0" aria-label="A ${room.roomType} with ${room.numBed} bed(s) that is $${room.costPerNight.toFixed(2)} per night" class="room-card">
     ${roomDate}  
@@ -90,17 +94,25 @@ export const showCalendar = () => {
   show(viewCalendar)
 }
 
+export const showTypeMenu = () => [
+  menuHeader.innerHTML = `Filter`
+] 
+
 export const bookedMessage = () => {
   return setTimeout(() => {
-    removeRoom();
+    showAltView();
     dynamicMsg.innerHTML = 'Booked!'
     show([portal, guestMenu])
-    hide([bookingMsg])
+    hide([altView])
   }, 5000)
 }
 
-export const removeRoom = () => {
+export const showAltView = (filter) => {
+  if (filter === 'Type') {
+    altMsg.innerHTML = `search by room type`
+    
+  }
   hide([availableRoomsView, viewCalendar])
-  show([bookingMsg])
+  show([altView])
 } 
 
